4f66f162741bb677a1bd83aaac0a79afa8f13df0
estrellafel <felixestrella7@gmail.com>
2023-02-02 14:22:35 -0600

    Added 3d model

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './About.css'
 import ProfileImage from './ProfileImage';
+import glb from '../assets/3d.glb'
 
 class About extends React.Component {
     render() {
@@ -43,6 +44,12 @@ class About extends React.Component {
                     Node.js, and Python. I also wrote documentation for software components related to augmented reality and managed various 3D models that were implemented 
                     into a program.
                 </p>
+                <model-viewer
+                    src={glb}
+                    alt="A rock"
+                    camera-controls
+                    ar>
+                </model-viewer>
             </div>
         );
     }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,7 @@ import Home from './Home.js';
 import About from './About.js';
 import Work from './Work.js';
 import Project from './Project.js';
+import AugmentedReality from './AugmentedReality.js';
 import './Navbar.css';
 
 class Navbar extends React.Component {
@@ -21,12 +22,14 @@ class Navbar extends React.Component {
                     <li><Link to={'/reactResume/about'} className='nav-link'>About</Link></li>
                     <li><Link to={'/reactResume/experience'} className='nav-link'>Work Experience</Link></li>
                     <li><Link to={'/reactResume/projects'} className='nav-link'>Projects</Link></li>
+                    <li><Link to={'/reactResume/augmentedReality'} className='nav-link'>Augmented Reality</Link></li>
                 </ul>
                 <Routes>
                     <Route path='reactResume' element={<Home/>}/>
                     <Route path='reactResume/about' element={<About/>}/>
                     <Route path='reactResume/experience' element={<Work/>}/>
                     <Route path='reactResume/projects' element={<Project/>}/>
+                    <Route path='reactResume/augmentedReality' element={<AugmentedReality/>}/>
                     <Route path='' element={<Navigate to='/reactResume' />} />
                 </Routes>
             </HashRouter>
