a0502617472928200ec7cc5742812092076e3865
estrellafel <felixestrella7@gmail.com>
2025-03-18 16:36:22 -0500

    format to work section update

--- src/components/ResumeSection/Work.js
+++ src/components/ResumeSection/Work.js
@@ -10,26 +10,38 @@ class Work extends React.Component {
                 <div>
                     <h2><b>Atlantic Casualty Insurance Company, Goldsboro, NC</b></h2>
                     <h2>Software Engineer II | 01/2024 - Present</h2>
+                    <ul>
+                        <li>Developed and optimized features in C# and legacy VB6 applications, streamlining 
+                            policy processing and premium distribution for $500 million annually across SQL 
+                            Server and Oracle databases</li>
+                        <li>Designed and implemented automated target premium logic, helping agents and 
+                            underwriters make optimal decisions for policyholders</li>
+                        <li>Spearheaded the migration of a policy processing application from VB6 to C#, 
+                            cutting processing time by 85% and significantly improving accuracy</li>
+                        <li>Integrated multiple wildfire risk APIs into the company’s rating application, 
+                            enabling hazard assessment and enhancing underwriting accuracy by storing and 
+                            leveraging wildfire data</li>
+                        <li>Optimized and updated Oracle database functions, views, and stored procedures, 
+                            enhancing data accuracy and efficiency for daily reporting processes</li>
+                        <li>Led a garage project implementing statutory UIM/UM rules for multiple states, 
+                            ensuring compliance within the company’s rating application</li>
+                        <li>Integrated hail-prone state classifications into the comapny's rating application, 
+                            allowing for accurate risk assessment and proper rating of weather-related exposures</li>
+                        <li>Mentored and onboarded new engineers, providing hands-on training in the company’s 
+                            proprietary rating system and policy issuance software</li>
+                    </ul>
                     <h2>Software Engineer I | 10/2022 - 01/2024</h2>
                     <ul>
-                        <li>Engineered a database-driven form data management system using C#, ASP.NET, Angular, and Oracle, 
-                            reducing manual updates and improving efficiency for analysts and engineers</li>
-                        <li>Developed and optimized features in C# and legacy VB6 applications, streamlining policy 
-                            processing and premium distribution for $500 million annually across SQL Server and Oracle databases</li>
-                        <li>Designed and implemented automated target premium logic, helping agents and underwriters make optimal decisions 
-                            for policyholders</li>
-                        <li>Implemented a proactive data validation feature for a REST API, preventing erroneous ratings and delivering actionable 
-                            feedback for insurance personnel</li>
-                        <li>Spearheaded the migration of a policy processing application from VB6 to C#, cutting processing time by 85% 
-                            and significantly improving accuracy</li>
-                        <li>Led onboarding for new engineers, providing hands-on training in the company's proprietary 
-                            rating system and policy issuance software</li>
+                        <li>Engineered a database-driven form data management system using C#, ASP.NET, Angular, 
+                            and Oracle, reducing manual updates and improving efficiency for analysts and engineers</li>
+                        <li>Implemented a proactive data validation feature for a REST API, preventing erroneous ratings 
+                            and delivering actionable feedback for insurance personnel</li>
                         <li>Integrated new insurance coverages into an existing .NET application using Duck Creek, broadening 
                             available P&C insurance options</li>
-                        <li>Integrated multiple wildfire risk APIs into the company’s rating application, enabling hazard assessment and 
-                            enhancing underwriting accuracy by storing and leveraging wildfire data</li>
-                        <li>Diagnosed and resolved CI/CD pipeline failures in Jenkins, ensuring smooth and 
-                            timely software deployments</li>
+                        <li>Refactored and modernized business critical policy data collection tooling, improving 
+                            maintainability and performance within the .NET application</li>
+                        <li>Diagnosed and resolved CI/CD pipeline failures in Jenkins, ensuring smooth and timely 
+                            software deployments</li>
                     </ul>
                 </div>
                 <div>
